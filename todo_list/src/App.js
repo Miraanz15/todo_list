8ab95656cdb5233f7388164cf1fe7f90952bebc7
Miraanz15 <miraanejaz15@gmail.com>
2023-10-20 02:14:31 +0530

    All done

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -5,6 +5,7 @@ import Taskbar from './Taskbar';
 function App() {
   const [value, setValue] = useState('');
   const [components, setComponents] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const handleChange = (event) => {
      console.log(event.target.value);
@@ -14,6 +15,7 @@ function App() {
    const handleSubmit = (event) =>{
     event.preventDefault();
     console.log(`value = ${value}`); 
+    setTotal(total + 1);
     const newComponent = <Taskbar value={value}/>;
     setComponents([...components, newComponent]);
     setValue('');
@@ -22,6 +24,7 @@ function App() {
   return (
     <div> 
     <div className={styles.App}>
+        <h1 style={{color:"gray"}}>Tasks</h1>
         <form className={styles.taskAdder} onSubmit={handleSubmit}>
           <input className={styles.addValue} type = "text" value = {value} placeholder = "   Enter Task" onChange ={handleChange}/>
           <button className={styles.addButton}>Add</button>
@@ -29,6 +32,9 @@ function App() {
         {components.map((component, index) => (
         <div key={index}>{component}</div>
       ))}
+      <div className={styles.total}>
+         Total Tasks {total}
+      </div>
     </div>
     </div>
   );
